7c8df931bf366da554c6e40ff88211090fb79ff2
Alice <devo4ka2410@gmail.com>
2022-05-13 16:28:05 +0300

    feat: add config for DynamoDB
    create table
    
    MODULE3-PART2

---
+++ config/serverless/parts/DBConf/table.ts
@@ -0,0 +1,78 @@
+import { AWSPartitial } from '../../types';
+
+export const galleryTableConfig: AWSPartitial = {
+  provider: {
+    environment: {
+      USERS_TABLE_NAME: '${self:custom.tablesNames.UsersTable.${self:provider.stage}}',
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: [
+              'dynamodb:DescribeTable',
+              'dynamodb:Query',
+              'dynamodb:Scan',
+              'dynamodb:GetItem',
+              'dynamodb:PutItem',
+              'dynamodb:DeleteItem',
+              'dynamodb:UpdateItem',
+              'dynamodb:BatchGetItem',
+              'dynamodb:BatchWriteItem',
+            ],
+            Resource: [
+              'arn:aws:dynamodb:*:*:table/${self:custom.tablesNames.UsersTable.${self:provider.stage}}',
+              'arn:aws:dynamodb:*:*:table/${self:custom.tablesNames.UsersTable.${self:provider.stage}}/index/*',
+            ],
+          },
+        ],
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      UsersTable: {
+        Type: 'AWS::DynamoDB::Table',
+        DeletionPolicy: 'Retain',
+        Properties: {
+          AttributeDefinitions: [
+            {
+              AttributeName: 'partitionKey',
+              AttributeType: 'S',
+            },
+            {
+              AttributeName: 'sortKey',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'partitionKey',
+              KeyType: 'HASH',
+            },
+            {
+              AttributeName: 'sortKey',
+              KeyType: 'RANGE',
+            },
+          ],
+          BillingMode: 'PAY_PER_REQUEST',
+          TableName: '${self:custom.tablesNames.UsersTable.${self:provider.stage}}',
+          StreamSpecification: {
+            StreamViewType: 'NEW_AND_OLD_IMAGES',
+          },
+        },
+      },
+    },
+  },
+  custom: {
+    tablesNames: {
+      UsersTable: {
+        local: 'Alice-Users-local',
+        dev: 'Alice-Users-dev',
+        test: 'Alice-Users-test',
+        prod: 'Alice-Users',
+      },
+    },
+  },
+};
