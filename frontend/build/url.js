22ce3c86c8773bc9492d68aa429223fec15714dc
Alice <devo4ka2410@gmail.com>
2022-05-19 11:44:48 +0300

    refactor: delete files
    delete redundant files
    
    MODULE3-PART2

--- frontend/build/url.js
+++
@@ -1,5 +0,0 @@
-export const loginURL = 'http://localhost:5000/authorization';
-export const signUpUrl = 'http://localhost:5000/signup';
-export const basicGalleryURL = 'http://localhost:5000/gallery';
-export const localStorageTokenKey = 'token';
-export const tokenTimestampKey = 'tokenReceiptTime';
